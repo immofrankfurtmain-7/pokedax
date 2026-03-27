4b08f9f92a43fb0c5dfd48b8686f01b943b324f1
immofrankfurtmain-7 <immofrankfurtmain@gmail.com>
2026-03-27 10:44:29 +0100

    Update page.tsx

--- src/app/preischeck/page.tsx
+++ src/app/preischeck/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect, useCallback } from 'react'
 import Image from 'next/image'
 import { useDebounce } from '@/hooks/useDebounce'
 import WishlistButton from '@/components/ui/WishlistButton'
-import WishlistButton from '@/components/ui/WishlistButton'
+
 
 
 type Card = {
