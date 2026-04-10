bf9024977a4b44c106d007f462777963fac4e31e
immofrankfurtmain-7 <immofrankfurtmain@gmail.com>
2026-04-10 11:40:41 +0200

    dashi3

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,4 +1,8 @@
-﻿import { createClient as c } from "@supabase/supabase-js";
+﻿import { createBrowserClient } from "@supabase/ssr";
+
 export function createClient() {
-  return c(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
 }
